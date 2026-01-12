35350238f77458f975d374fd5d73457cc240e5c2
pranulkbv28 <28bvpranaav@gmail.com>
2026-01-12 17:51:38 +0530

    Implement main method in IntroToQueues to demonstrate Queue operations

--- DSA/DataStructures/QueuesInJava/IntroToQueues.java
+++ DSA/DataStructures/QueuesInJava/IntroToQueues.java
@@ -102,5 +102,22 @@ class Queue {
 }
 
 public class IntroToQueues {
+    public static void main(String[] args) throws QueueFullException {
+        Queue queue = new Queue(3);
+        int arr[] = { 10, 20, 30, 40 };
+        for (int elem : arr) {
+            // try {
+            // } catch (QueueFullException e) {
+            // }
 
+            queue.enqueue(elem);
+        }
+
+        while (!queue.isEmpty()) {
+            try {
+                System.out.print(queue.dequeue() + " ");
+            } catch (QueueEmptyException e) {
+            }
+        }
+    }
 }
